1aa92d7c52a4530b9407e271a04ace5281848683
chenxin <chen.xin@hbtown.com>
2017-12-21 15:14:31 +0800

    init

---
+++ app/app.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Local variables
+ * @var \Phalcon\Mvc\Micro $app
+ */
+
+/**
+ * Add your routes here
+ */
+$app->get('/', function () {
+    echo $this['view']->render('index');
+});
+
+/**
+ * Not found handler
+ */
+$app->notFound(function () use($app) {
+    $app->response->setStatusCode(404, "Not Found")->sendHeaders();
+    echo $app['view']->render('404');
+});
+
+$app->get('/hello', function () {
+    echo '<pre>';
+    echo shell_exec('php -m');
+});
